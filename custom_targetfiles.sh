86ac3921c3cf81a4c1bde87f53a5bf1732107217
Diorgenes <dyogenes_gd@hotmail.com>
2017-05-04 11:53:27 -0300

    Updated files ...not necessary other/file_contexts ...updated file releasetools.py

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -1,4 +1,3 @@
 #/bin/bash
 TARGET_DIR=out/merged_target_files
 
-cp -f other/file_contexts $TARGET_DIR/META/
--- releasetools.py
+++ releasetools.py
@@ -1,38 +1,44 @@
 import common
 import edify_generator
 
-def RemoveDeviceAssert(info):
-  edify = info.script
-  for i in xrange(len(edify.script)):
-    if "ro.product" in edify.script[i]:
-      edify.script[i] = """assert(getprop("ro.product.device") == "hammerhead" || getprop("ro.build.product") == "hammerhead" || abort("This package is for device: hammerhead; this device is " + getprop("ro.product.device") + "."););
-unmount("/data");
-unmount("/system");"""
-      return
 
-def ModifyMountData(edify):
+def ModifyBegin(edify):
     for i in xrange(len(edify.script)):
-        if "mount(" in edify.script[i] and "by-name/userdata" in edify.script[i]:
-            edify.script[i] = 'run_program("/sbin/busybox", "mount", "/data");'
+        if 'ui_print("Target:' in edify.script[i] and "user/test-keys" in edify.script[i]:
+            edify.script[i] = '''assert(getprop("ro.product.device") == "hammerhead" || getprop("ro.build.product") == "hammerhead" || abort("This package is for device: hammerhead; this device is " + getprop("ro.product.device") + "."););
+unmount("/system");
+ui_print("===============================================");
+ui_print("      Flyme 6 International for Nexus 5        ");
+ui_print("===============================================");
+ui_print("            By Dio_S @XDADevelopers            ");
+ui_print("===============================================");
+ui_print("===============================================");
+ui_print("DEVICE: Google Nexus 5                         ");
+ui_print("FLYME 6 VERSION:                               ");
+ui_print("ANDROID VERSION: 6.0.1                         ");
+ui_print("===============================================");'''
 
-def ModifyMountSystem(edify):
+
+def ModifyCommand(edify):
     for i in xrange(len(edify.script)):
-        if "mount(" in edify.script[i] and "by-name/system" in edify.script[i]:
-            edify.script[i] = 'mount("ext4", "EMMC", "/dev/block/platform/msm_sdcc.1/by-name/system", "/system");'
+        if "package_extract_dir(" in edify.script[i] and "recovery" in edify.script[i]:
+            edify.script[i] = 'ui_print("Installing system...");'
 
-def WritePolicyConfig(info):
-  try:
-    file_contexts = info.input_zip.read("META/file_contexts")
-    common.ZipWriteStr(info.output_zip, "file_contexts", file_contexts)
-  except KeyError:
-    print "warning: file_context missing from target;"
+
+def AddPrompt(edify):
+    for i in xrange(len(edify.script)):
+        if "format(" in edify.script[i] and "by-name/system" in edify.script[i]:
+            edify.script[i] = 'ui_print("Formating Partition...");\n' + edify.script[i]
+        elif 'symlink("/system/lib/libbluetooth_jni.so' in edify.script[i]:
+            edify.script[i] = 'ui_print("Creating symlinks...");\n' + edify.script[i]
+        elif 'set_metadata_recursive("/system"' in edify.script[i]:
+            edify.script[i] = 'ui_print("Setting permissions...");\n' + edify.script[i]
+        elif 'package_extract_file("boot' in edify.script[i]:
+            edify.script[i] = 'ui_print("Flashing Kernel...");\n' + edify.script[i]
 
 
 def FullOTA_InstallEnd(info):
     edify = info.script
-    ModifyMountData(edify)
-    WritePolicyConfig(info)
-    RemoveDeviceAssert(info)
-
-def IncrementalOTA_InstallEnd(info):
-    RemoveDeviceAssert(info)
+    ModifyBegin(edify)
+    ModifyCommand(edify)
+    AddPrompt(edify)
