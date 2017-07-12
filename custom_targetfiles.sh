819a76884a2565d40e549a19f9d51ec44a628415
Diorgenes <dyogenes_gd@hotmail.com>
2017-07-12 19:09:19 -0300

    Adding settings to gapps

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -1,3 +1,13 @@
 #/bin/bash
-TARGET_DIR=out/merged_target_files
 
+PWD=`pwd`
+TARGET_DIR=out/merged_target_files
+GAPPS=$PWD/gapps
+
+#Added Gapps to ROM
+if [ -e $GAPPS/SYSTEM ]
+then
+   echo ">>> Adding Gapps to ROM"
+   cp -rf $GAPPS/SYSTEM $TARGET_DIR
+   echo ">>> Gapps added"
+fi
